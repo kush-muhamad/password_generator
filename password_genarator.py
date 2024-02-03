ea54f48ebf2659d6e2e28685e0e49349f1f14639
kush Muhamad <kushmohammed98@yahoo.com>
2024-02-03 03:07:02 +0300

    removed some print issues

--- password_genarator.py
+++ password_genarator.py
@@ -26,9 +26,9 @@ for sym in range(1, nr_symbols + 1):
 for num in range(1, nr_numbers + 1):
     password_list += random.choice(numbers)
 
-print(password_list)
+
 random.shuffle(password_list)
-print(password_list)
+
 password = ""
 for char in password_list:
     password += char
